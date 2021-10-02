c0289e64c57522769539d1bc275ab5dc83b9ace6
Kure pon <crackernut47@gmail.com>
2021-10-02 22:35:46 +0700

    Membuat Recyclerview tahap awal

--- app/src/main/java/com/grntea/ygocard/MainActivity.java
+++ app/src/main/java/com/grntea/ygocard/MainActivity.java
@@ -1,13 +1,43 @@
 package com.grntea.ygocard;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    RecyclerView.Adapter recyclerViewAdapter;
+    RecyclerView.LayoutManager recylerViewLayoutManager;
+    ArrayList<ItemModel> data;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+
+        recylerViewLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(recylerViewLayoutManager);
+        recyclerView.addItemDecoration(new DividerItemDecoration(this, LinearLayoutManager.VERTICAL));
+
+        data = new ArrayList<>();
+        for (int i = 0; i < 20; i++) {
+            data.add(new ItemModel(
+                    MyItem.Headline[0],
+                    MyItem.Subhead[0],
+                    MyItem.iconList[0]
+            ));
+        }
+
+        recyclerViewAdapter = new AdapterRecyclerView(data);
+        recyclerView.setAdapter(recyclerViewAdapter);
     }
 }
---
+++ app/src/main/java/com/grntea/ygocard/MyItem.java
@@ -0,0 +1,11 @@
+package com.grntea.ygocard;public class MyItem {
+
+    static int[] iconList = {
+            R.drawable.card_back
+    };
+
+    static String[] Headline = {"Title"};
+
+    static String[] Subhead = {"Description"};
+}
+
