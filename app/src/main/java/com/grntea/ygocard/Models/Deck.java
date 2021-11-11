1a01ae1debd99c7ec2cc26fad3cb0638867fe382
Kure pon <crackernut47@gmail.com>
2021-11-11 09:52:54 +0700

    ngerapihin kode Dao

--- app/src/main/java/com/grntea/ygocard/Models/Deck.java
+++ app/src/main/java/com/grntea/ygocard/Models/Deck.java
@@ -4,7 +4,7 @@ import androidx.room.Entity;
 import androidx.room.Ignore;
 import androidx.room.PrimaryKey;
 
-@Entity(tableName = "Fav")
+@Entity(tableName = "Deck")
 public class Deck {
 
     @PrimaryKey(autoGenerate = true)
